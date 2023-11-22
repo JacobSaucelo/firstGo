8857715efce0465ad823d49c3c63dcc952f51597
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-11-22 14:50:59 +0800

    datatypesss

---
+++ testingFmt.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	textt := "coming in the air"
+	ton := "tonight"
+	var shitt32 float64 = 420.6969
+
+	fmt.Print("I can feel it ", textt)
+	fmt.Println("tonight")
+
+	fmt.Printf("i can feel it coming in the air %v \n", ton)
+	fmt.Printf("poopypants: %v %0.1f %T \n", ton, 20.20, ton)
+
+	henlo := fmt.Sprintf("haii!! ^_^ hiii!!! :3 dis U?:%T", shitt32)
+	fmt.Println(henlo)
+}

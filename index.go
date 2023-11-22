29423cdb728dd377dd8fee95151abffab2649a50
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-11-22 14:31:24 +0800

    first go commit

---
+++ index.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+var shit2 string = "asdf2"
+
+func main() {
+
+	var shit string = "asdf"
+	shitter := "weelll"
+	shit = "bagoshit"
+	fmt.Println(shit, shit2, shitter)
+
+	numberSjit := 12
+	var numberSjitF float32 = 10.232
+	var coolInt int8 = 127
+	var coolInt2 uint8 = 1
+
+	fmt.Println(numberSjit + int(numberSjitF))
+	fmt.Println(coolInt, coolInt2)
+
+	var hakdog byte = 120
+	var bruh rune = 42069
+
+	fmt.Println(hakdog, bruh)
+
+}
